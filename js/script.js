65d2484ab702397d5de3f1e08a378ac072214f3a
Amy Tomey <amy.tomey59@gmail.com>
2021-04-01 12:29:03 -0400

    add hide menu in js, removed errors found in console log

--- js/script.js
+++ js/script.js
@@ -1,20 +1,6 @@
-const name = document.getElementById('name')
-const mail = document.getElementById('mail')
-const form = document.getElementById('form')
-const errorElement = document.getElementById('error')
-const emailChecker =  /\S+@\S+\.\S+/;
-
-form.addEventListener('submit', (e) => {
-    let messages = []
-    if (name.value === '' || name.value === null) {
-      messages.push("Pleease enter your name");
-    }
-    if (mail.value === '' || mail.value === null ) {
-        messages.push("Pleease enter a valid email");
-      }
-    
-    if (messages.length > 0) {
-      e.preventDefault()
-      errorElement.innerText = messages.join (` , `)
-    }
-})
+function displayHide() {
+    document.getElementById("menu").style.visibility = "hidden";
+ }
+ function displayShow() {
+    document.getElementById("menu").style.visibility = "visible";
+ }
